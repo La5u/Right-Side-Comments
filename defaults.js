8e88a3c119442ed9308157ed73a350225e5e494e
la5u <lasushin@proton.me>
2026-03-27 01:00:12 +0500

    defaults: add extension mode and fullscreen settings

--- defaults.js
+++ defaults.js
@@ -1,12 +1,14 @@
 // Shared defaults for popup, content script, and background.
 self.RSC_DEFAULTS = {
-  sidebarEnabled: true,
+  extensionEnabled: true,
+  sidebarMode: "default",
   autoExpand: true,
   showRelated: true,
   innerScrollbar: true,
   outerScrollbar: false,
   compactMargins: true,
   staticCommentBox: true,
+  fullscreenComments: false,
   commentsWidth: null,
   hideSideMargins: false,
   themeOverride: null,
